d1dfa9a4f981b01fc6a0108ea89e20638ced0746
Maria Pokasova <mpokasova99@gmail.com>
2025-08-15 01:40:44 +0300

    commit add-database

--- src/main/java/ru/yandex/practicum/filmorate/dao/mappers/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/mappers/UserRowMapper.java
@@ -12,6 +12,7 @@ public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
         User user = new User();
+
         user.setId(resultSet.getLong("id"));
         user.setEmail(resultSet.getString("email"));
         user.setLogin(resultSet.getString("login"));
