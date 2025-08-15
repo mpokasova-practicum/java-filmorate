7c166c380d517fb586b5b1ac852a22eb6626f6a0
Maria Pokasova <mpokasova99@gmail.com>
2025-08-15 11:51:47 +0300

    commit fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -31,15 +31,15 @@ INSERT INTO film (name, description, releaseDate, duration, mpa_id) VALUES
 ('Фильм 4', 'Описание фильма 4', '2015-12-25', 110, 4),
 ('Фильм 5', 'Описание фильма 5', '2020-07-07', 180, 5);
 
--- Вставка данных в таблицу film_genres (жанры фильмов)
+---- Вставка данных в таблицу film_genres (жанры фильмов)
 --INSERT INTO film_genres (film_id, genre_id) VALUES
 --(1, 1), (1, 2),
 --(2, 3), (2, 4),
 --(3, 5), (3, 6),
 --(4, 1), (4, 3),
 --(5, 2), (5, 4);
-
--- Вставка данных в таблицу user_friendship (дружба пользователей)
+--
+---- Вставка данных в таблицу user_friendship (дружба пользователей)
 --INSERT INTO user_friendship (user_id, friend_id, confirmed) VALUES
 --(1, 2, true),
 --(1, 3, false),
@@ -47,8 +47,8 @@ INSERT INTO film (name, description, releaseDate, duration, mpa_id) VALUES
 --(3, 4, true),
 --(4, 5, false),
 --(5, 1, true);
-
--- Вставка данных в таблицу film_likes (лайки фильмов)
+--
+---- Вставка данных в таблицу film_likes (лайки фильмов)
 --INSERT INTO film_likes (film_id, user_id) VALUES
 --(1, 1), (1, 2), (1, 3),
 --(2, 2), (2, 4),
